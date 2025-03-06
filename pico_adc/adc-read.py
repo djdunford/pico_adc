b8cc2d2924133375052fcab383bfc1a8079fc3a7
Darren Dunford <45966937+djdunford@users.noreply.github.com>
2025-03-06 17:05:27 +0000

    add in neopixel

--- pico_adc/adc-read.py
+++ pico_adc/adc-read.py
@@ -1,9 +1,14 @@
 import machine
 import utime
- 
+import neopixel
+
+neoString = neopixel.NeoPixel(machine.Pin(8), 20)
 analog_value_x = machine.ADC(28)
 analog_value_y = machine.ADC(27)
 button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
+
+neoString.fill((100,0,0))
+neoString.write()
  
 while True:
     reading_x = analog_value_x.read_u16()     
